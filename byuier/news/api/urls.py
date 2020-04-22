96f716becd5d6e3d311082ebabe956a3bd9258f5
Arnel Imperial <engine@Arnels-MacBook-Pro.local>
2020-04-22 07:38:48 +0300

    Update news/api/views with CRUD functionality

--- byuier/news/api/urls.py
+++ byuier/news/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from byuier.news.api.views import article_list_create_api_view
+from byuier.news.api.views import (
+    article_list_create_api_view,
+    article_detail_api_view
+)
 
 urlpatterns = [
-    path('articles/', article_list_create_api_view, name='article-list')
+    path('articles/', article_list_create_api_view, name='article-list'),
+    path('articles/<int:pk>/', article_detail_api_view, name='article-detail')
 ]
